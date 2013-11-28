35c1b956c31844941e3102f03336e773fba5d69d
mkeung <mikekeung@gmail.com>
2013-11-28 16:06:47 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mikekeung::Application.config.secret_key_base = 'e5003c572f9f3ac32db376c8da7e0e9b7fb12626586aaeaa43271b88828231f0208f96a4f5f150923579cd6c8378c24254237d49516cdb10a39f8be269eafcca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mikekeung::Application.config.session_store :cookie_store, key: '_mikekeung_session'
